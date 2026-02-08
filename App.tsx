f9bb5e4d8c8bce4c1a85f2f68423b1cd9939f94b
Harsh Bairagi <bairagiharsh092@gmail.com>
2026-02-08 17:59:18 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Hero from './sections/Hero';
+import Challenge from './sections/Challenge';
+import Approach from './sections/Approach';
+import Progress from './sections/Progress';
+import Differentiation from './sections/Differentiation';
+import Mission from './sections/Mission';
+import Contact from './sections/Contact';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <Navbar />
+      </header>
+      <main id="main-content" className="flex-grow focus:outline-none" tabIndex={-1}>
+        <Hero />
+        <Challenge />
+        <Approach />
+        <Progress />
+        <Differentiation />
+        <Mission />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
